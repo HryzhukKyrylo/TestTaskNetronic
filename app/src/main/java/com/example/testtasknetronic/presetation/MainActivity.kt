e4cb9c65b9e3baa330a9c7c4f6393f8e844000c0
Kyrylo <kyrylo.hryzhuk.natife@gmail.com>
2022-12-15 13:44:34 +0200

    add dependency

--- app/src/main/java/com/example/testtasknetronic/presetation/MainActivity.kt
+++ app/src/main/java/com/example/testtasknetronic/presetation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testtasknetronic
+package com.example.testtasknetronic.presetation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testtasknetronic.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
