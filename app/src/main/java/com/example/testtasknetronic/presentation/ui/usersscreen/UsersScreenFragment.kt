c031a4f15cf0eaa3a11a697c2e11faaf756fd1c8
Kyrylo <kyrylo.hryzhuk.natife@gmail.com>
2022-12-16 10:59:05 +0200

    add main menu

--- app/src/main/java/com/example/testtasknetronic/presentation/ui/usersscreen/UsersScreenFragment.kt
+++ app/src/main/java/com/example/testtasknetronic/presentation/ui/usersscreen/UsersScreenFragment.kt
@@ -2,10 +2,14 @@ package com.example.testtasknetronic.presentation.ui.usersscreen
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testtasknetronic.databinding.FragmentUsersScreenBinding
 import com.example.testtasknetronic.domain.model.UserModel
@@ -18,12 +22,46 @@ import com.example.testtasknetronic.utils.showLongToast
 import com.example.testtasknetronic.utils.showShortToast
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class UsersScreenFragment : BaseFragment<FragmentUsersScreenBinding>() {
     private lateinit var recycler: RecyclerView
     private lateinit var adapter: MainAdapter
+    private lateinit var menuItem: MenuItem
 
     private val viewModel: UsersViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // First clear current all the menu items
+        menu.clear();
+
+        // Add the new menu items
+        inflater.inflate(com.example.testtasknetronic.R.menu.main_menu, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+//            com.example.testtasknetronic.R.id.historyScreenFragment -> {
+//                requireContext().showShortToast("History")
+//                return true
+//            }
+            com.example.testtasknetronic.R.id.action_changeTheme -> {
+                requireContext().showShortToast("Theme")
+                return true
+            }
+            else -> {}
+        }
+        return item.onNavDestinationSelected(findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initClickListeners()
